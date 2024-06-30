08804cdb3ce5550e9bb8266b94ed8cc5e7698c4d
goodnessohi <76630943+goodnessohi@users.noreply.github.com>
2024-06-30 09:00:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,19 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        pass
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity= request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch= request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            writing_score=float(request.form.get('writing_score')),
+            reading_score=float(request.form.get('reading_score'))
+        )
+        pred_df = data.get_data_as_df()
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        
+        return render_template('home.html', results=results[0])
