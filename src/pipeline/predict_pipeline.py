842aa2499af25547bd687449043d755367d023e4
goodnessohi <76630943+goodnessohi@users.noreply.github.com>
2024-07-02 10:18:15 +0100

    An improvement to the html code to ensure presentability and functionality

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -7,6 +7,25 @@ from src.utils import load_object
 from src.logger import logging
 
 
+class PredictPipeline:
+    def __init__(self):
+         pass
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            
+            return preds
+        
+        except Exception as e:
+            raise CustomException(str(e))  
+##This class is responsible for mapping the input data from the html front end to the pipeline behind
 class CustomData:
     def __init__(self,
                   gender: str,
@@ -17,13 +36,13 @@ class CustomData:
                   reading_score: int,
                   writing_score: int
                   ):
-        self.gender = self.check_missing(gender, "Unknown")
-        self.race_ethnicity = self.check_missing(race_ethnicity, "Unknown")
-        self.parental_level_of_education = self.check_missing(parental_level_of_education, "Unknown")
-        self.lunch = self.check_missing(lunch, "Unknown")
-        self.test_preparation_course = self.check_missing(test_preparation_course, "Unknown")
-        self.reading_score = self.check_missing(reading_score, 0)
-        self.writing_score = self.check_missing(writing_score, 0)
+        self.gender = self.check_missing(gender, "female")
+        self.race_ethnicity = self.check_missing(race_ethnicity, "group D")
+        self.parental_level_of_education = self.check_missing(parental_level_of_education, "Bachelor's Degree")
+        self.lunch = self.check_missing(lunch, "Standard")
+        self.test_preparation_course = self.check_missing(test_preparation_course, "Completed")
+        self.reading_score = self.check_missing(reading_score, 69)
+        self.writing_score = self.check_missing(writing_score, 68)
         
     def check_missing(self, value, default):
         if value is None or value == "":
